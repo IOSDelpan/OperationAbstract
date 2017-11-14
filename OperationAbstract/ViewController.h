388d177cd70aa30c9fdf08b92ce8f68340849011
IOSDelpan <delpan@DelpandeMac-mini.local>
2017-11-14 15:23:34 +0800

    Initial Commit

---
+++ OperationAbstract/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OperationAbstract
+//
+//  Created by Delpan on 2017/11/14.
+//  Copyright © 2017年 Delpan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OperationAbstract/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OperationAbstract
+//
+//  Created by Delpan on 2017/11/14.
+//  Copyright © 2017年 Delpan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
